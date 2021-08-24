5d088e899780cbfe16e5dcf436976c376d1be8a5
Dalmo Silva Mendes <65993036+DalmoMendes@users.noreply.github.com>
2021-08-24 00:19:22 -0300

    Declaração de Variáveis e saída

--- aula02.php
+++ aula02.php
@@ -1,11 +1,20 @@
 <link rel="stylesheet" href="style.css">
-<?php /*CeproiRR - Centro Profissional de Informática
-		Curso Web Developer PHP - Fullstack Macuxi
-		Professor: Dalmo Silva Mendes, Boa Vista-RR, 2021 */ ?>
 <?php
-// Aula 02
+/* CEPROIRR - CENTRO PROFISSIONAL DE INFORMÁTICA
+ * SEJA BEM-VINDO AO CURSO DE PHP BÁSICO.
+ * CURSO CRIADO POR DALMO SILVA MENDES
+ * SITE: CEPROIRR.COM.BR
+ * Fazer Doação
+ * Banco do Brasil
+ * Agência: 0250-X
+ * Conta: 87.489-2
+ */
+/*
+  Aula: Declaração de Variaveis e saída
+ * Testado no PHP7,8
+ */
 
-$ola = "Olá!";
+$ola1 = "Olá!";
 $primeiro_nome = "Dalmo";
 $segundoNome = "Silva";
 $_1cidade = "Boa Vista";
@@ -16,14 +25,14 @@ $FONE = "(95)99120-6763";
 
 echo "<div class='php'>";
 
-echo "<h1>$ola</h1>";
+echo "<h1>$ola1</h1>";
 echo "Meu nome é $primeiro_nome $segundoNome,
  moro na cidade de $_1cidade-$_2uf. 
  meu contato é $tel, $Tel e $FONE";
 
 echo "<hr>";
 
-$ola = "Olá!";
+$ola2 = "Olá!";
 $usu_primeiro_nome = "Dalmo";
 $usu_segundo_nome = "Silva";
 $usu_cidade = "Boa Vista";
@@ -31,10 +40,9 @@ $usu_uf = "RR";
 $usu_tel = "(95)-991206763";
 
 
-echo "<h1>$ola</h1>";
+echo "<h1>$ola2</h1>";
 echo "Meu nome é $usu_primeiro_nome $usu_segundo_nome,
  moro na cidade de $usu_cidade-$usu_uf. 
  Meu contato é $usu_tel";
 
 echo "</div>";
-?>
