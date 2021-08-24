23acf32d9c70ffbe4042c7cd386135b059f46271
Dalmo Silva Mendes <65993036+DalmoMendes@users.noreply.github.com>
2021-08-24 00:18:16 -0300

    Sintax do php, variáveis e saída

--- aula01.php
+++ aula01.php
@@ -1,16 +1,27 @@
 <link rel="stylesheet" href="style.css">
-<?php /*CeproiRR - Centro Profissional de Informática
-		Curso Web Developer PHP - Fullstack Macuxi
-		Professor: Dalmo Silva Mendes, Boa Vista-RR, 2021 */ ?>
 <?php
-// Aula 01
-$apresentacao = "Seja bem-vindo!";
-$curso = "Curso Web Developer PHP - Fullstack Macuxi";
+/* CEPROIRR - CENTRO PROFISSIONAL DE INFORMÁTICA
+ * SEJA BEM-VINDO AO CURSO DE PHP BÁSICO.
+ * CURSO CRIADO POR DALMO SILVA MENDES
+ * SITE: CEPROIRR.COM.BR
+ * Fazer Doação
+ * Banco do Brasil
+ * Agência: 0250-X
+ * Conta: 87.489-2
+ */
+/*
+Aula: Sintax do php, variaveis e saída.
+ * Testado no PHP7,8
+*/
 
-echo "<div class='php'>";
+$apresentacao = '<h2>Seja bem-vindo!</h2>';
+$curso = '<p>Curso de PHP Básico.</p>';
 
-echo "<h1>$apresentacao</h1>";
-echo "<p>$curso</p>";
+echo '<div class="php">';
 
-echo "</div>";
-?>
+echo 'Aula 01:' . '<br>';
+echo $apresentacao;
+echo '<br>';
+echo $curso;
+
+echo '</div>';
